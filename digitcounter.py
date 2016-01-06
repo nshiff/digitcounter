ffbbddc9398f6dd2e448e644ab74ace1f3385935
N <nshiff@users.noreply.github.com>
2016-01-05 21:54:35 -0500

    auto-checking!

--- digitcounter.py
+++ digitcounter.py
@@ -1,25 +1,35 @@
 #self-referencial number solver
 # based on James Grime's puzzle
 # https://www.youtube.com/watch?v=K6Qc4oK_HqY
+#------------------------------------------------------------------------------
+from time import sleep
 
 
+guess_prev = '6210001000'	#arbitrary 10-digit num
+guess = ''
 
+solved = False
+while( not solved ):
+	sleep(1)
 
-guess = 4873498745 # any 10-digit number
+	counter = [0,0,0, 0,0, 0,0,0, 0,0]
+	for i in range(0, len(guess_prev)):
+		somedigit = int(guess_prev[i])
+		counter[somedigit] += 1
 
-guess_str = str(guess)
+	for i in range(0, len(counter)):
+		digit_as_string = str(counter[i])
+		guess += digit_as_string
 
-print(guess_str)
-
-
-counter = [0,0,0, 0,0, 0,0,0, 0,0]
-
-for i in range(0, len(guess_str)):
-	somedigit = int(guess_str[i])
-	counter[somedigit] += 1
-
-print(counter)
-
-
-
+	print('')
+	print(guess_prev)
+	print(counter)
+	print(guess)
+	
+	print(guess == guess_prev)
+	if(guess == guess_prev):
+		solved = True
+	else:
+		guess_prev= guess
+		guess = ''
 
