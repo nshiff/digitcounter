124f073e41a23f008f4238e889e33dc3a9f9abfc
N <nshiff@users.noreply.github.com>
2016-01-05 21:09:16 -0500

    hello digitcounter

---
+++ digitcounter.py
@@ -0,0 +1,12 @@
+#self-referencial number solver
+# based on James Grime's puzzle
+# https://www.youtube.com/watch?v=K6Qc4oK_HqY
+
+
+
+
+guess = 4873498745 # any 10-digit number
+
+print(guess)
+
+
